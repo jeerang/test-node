4a8adc6be085e5fd8b4d6953b1e931368e630918
jeerang <jeerang_wo@cmu.ac.th>
2025-11-29 16:56:59 +0700

    task10

--- src/for.ts
+++ src/for.ts
@@ -1,9 +1,4 @@
-let names = ['jeerang', 'toontoon', 'zea', 'tany', 'BMWwwww'];
-
-for (let i = 0; i < names.length; i++) {
-    if (names[i].length > 6) {
-        console.log('สวัสดีคุณ ' + names[i]);
-    } else {
-        console.log('Hello ' + names[i]);
-    }
+let color = ['red', 'blue', 'green', 'yellow', 'orange', 'purple']
+for (const index in color) {
+    console.log(`Index: ${index}, Value: ${color[index]}`);
 }
