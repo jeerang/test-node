7854ea1c961758a887af742f2a2a00614009479b
jeerang <jeerang_wo@cmu.ac.th>
2025-11-29 16:54:53 +0700

    task9

---
+++ src/for.ts
@@ -0,0 +1,9 @@
+let names = ['jeerang', 'toontoon', 'zea', 'tany', 'BMWwwww'];
+
+for (let i = 0; i < names.length; i++) {
+    if (names[i].length > 6) {
+        console.log('สวัสดีคุณ ' + names[i]);
+    } else {
+        console.log('Hello ' + names[i]);
+    }
+}
