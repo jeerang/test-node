bceb42b39232c294ce2f597b9e13c369edc903b4
jeerang <jeerang_wo@cmu.ac.th>
2025-11-29 16:27:45 +0700

    task8

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello, TypeScript with Node.js!");
+let score =55
+if (score > 80) {
+    console.log('Grade A')
+} else if (score > 70){
+    console.log('Grade B')
+} else if (score > 60){
+    console.log('Grade C')
+} else if (score > 50){
+    console.log('Grade D')
+}
+else {
+    console.log('Grade F')
+}
