9490879cbeca5b637107d04b103c582f2ef4bc29
jeerang <jeerang_wo@cmu.ac.th>
2025-12-06 09:48:42 +0700

    task12

--- src/function12.ts
+++ src/function12.ts
@@ -1,2 +1,7 @@
-const multiply = (x,y) => { return x * y }
-console.log(multiply(5, 10))
+
+export const multiply = (x: number, y: number): number => x * y;
+
+export const maxOfTwo = (a: number, b: number): number => (a > b ? a : b);
+
+
+console.log(maxOfTwo(5, 10));
