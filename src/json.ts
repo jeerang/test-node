cd56ff36d3a72a177c4ee2fe00b24877f34301de
jeerang <jeerang_wo@cmu.ac.th>
2025-12-12 09:26:02 +0700

    task14

---
+++ src/json.ts
@@ -0,0 +1,23 @@
+// src/json.ts
+type Student = {
+    firstName: string;
+    lastName: string;
+    age: number;
+    gpa: number;
+};
+
+const checkStudent = (student: Student): string => {
+    if (student.gpa > 2) {
+        return JSON.stringify(student);
+    }
+    return `คุณ ${student.firstName} พ้นสภาพ`;
+};
+
+// Example
+const s1: Student = { firstName: "Light", lastName: "Boy", age: 21, gpa: 2.5 };
+const s2: Student = { firstName: "Jeernag", lastName: "Wongwut", age: 22, gpa: 1.8 };
+
+console.log(checkStudent(s1));
+console.log(checkStudent(s2));
+
+exports.checkStudent = checkStudent;
